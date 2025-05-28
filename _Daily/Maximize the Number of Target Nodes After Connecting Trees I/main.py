738abfd96eb2d6b6e11f6eb685b0403ef337965d
unknown <farooqvrofficial@gmail.com>
2025-05-28 21:21:35 +0530

    25-05-2025

--- _Daily/Maximize the Number of Target Nodes After Connecting Trees I/main.py
+++ _Daily/Maximize the Number of Target Nodes After Connecting Trees I/main.py
@@ -2,52 +2,45 @@ from typing import List
 from collections import defaultdict, deque
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
-        adj1 = defaultdict(list)
-        adj2 = defaultdict(list)
 
-        n = len(edges1)+1        
-        m = len(edges2)+1        
-
-        for u, v in edges1:
-            adj1[u].append(v)
-            adj1[v].append(u)
+        def dfs(node, parent, children, k):
+            if k < 0:
+                return 0
+            
+            res = 1
+            for child in children[node]:
+                if child == parent:
+                    continue
+                res += dfs(child, node, children, k - 1)
+    
+            return res
         
-        for u, v in edges2:
-            adj2[u].append(v)
-            adj2[v].append(u)
 
-        max_cover_tree2 = 0
-        if k-1 == 0:
-            max_cover_tree2 = 1
-        else:
-            for node in range(m):
-                max_cover_tree2 = max(self.bfs(adj2, node, k), max_cover_tree2)
+        def build(edges, k):
+            n = len(edges)+1
+            children = [[] for _ in range(n)]
 
-        res = []
-        for node in range(n):
-            res.append(self.bfs(adj1, node, k+1)+max_cover_tree2)
+            for u, v in edges:
+                children[u].append(v)
+                children[v].append(u)
+            
+            res = [0] * n
+            for i in range(n):
+                res[i] = dfs(i, -1, children, k)
+
+            return res
+        
+        n = len(edges1) + 1
+        count1 = build(edges1, k)
+        count2 = build(edges2, k-1)
+        maxCount2 = max(count2)
+
+        res = [count1[i] + maxCount2 for i in range(n)]
+
         return res
 
-    def bfs(self, adjList, node, level):
 
-        q = deque([node])
-        length = 0
-        res = 0
-        visit = set([node])
 
-        while q and length < level:
-            for _ in range(len(q)):
-                node = q.popleft()
-                res +=1
-                for nei in adjList[node]:
-                    if nei in visit:
-                        continue
-                    visit.add(nei)
-                    q.append(nei)
-            length += 1
-        
-        return res
-        
 print(Solution().maxTargetNodes(
     [[0,1],[0,2],[2,3],[2,4]],
     [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]],
